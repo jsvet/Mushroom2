fe3792b3d5eac4ca872615a7db8a7dad322f6705
jsvet <Jessie@Jessies-MacBook-Pro.local>
2013-11-05 23:35:24 +0100

    stuck on imgs

--- js/moving-mushrooms.js
+++ js/moving-mushrooms.js
@@ -1,6 +1,10 @@
 var Game = {
 	stage : new createjs.Stage($("#stage")),
-	tileSrcs : ["img/small-mushroom.png", "img/small-dark-mushroom.png"]
+	tileSrcs : ["img/small-mushroom.png", "img/small-dark-mushroom.png"],
+	tiles : [],
+	level : [1, 0, 0, 0, 0],
+	offsetX : 25,
+	offsetY : 25
 };
 
 Game.Tile = function(myX, myY, myType){
@@ -12,12 +16,25 @@ Game.Tile = function(myX, myY, myType){
 };
 
 Game.update = function(){
+	var tile, i;
+	for (i =0; i< Game.tiles.length; i+=1){
+		tile = Game.tiles[i];
+		tile.x = tile.posX * tile.image.width + Game.offsetX;
+		tile.y = Game.offsetY;
+	}
 	Game.stage.update();
 };
 
 Game.initModel = function (){
-	var tile = new Game.Tile(0, 0, 0);
-	Game.stage.addChild(tile);
+	var col, type, i, tile, row = 0;
+	for (i = 0; i< Game.level.length; i+=1) {
+		console.log("adding tile");
+		type = Game.level[i];
+		col = i;
+		tile = new Game.Tile(col, row, type);
+		Game.tiles.push(tile);
+		Game.stage.addChild(tile);
+	}
 	Game.update();
 };
 
